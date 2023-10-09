a2e2091b856640535b63e216767da5651b78ea26
thach.do <ddthach95@gmail.com>
2023-10-10 01:22:06 +0800

    Restructured assets bundling

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -18,21 +18,28 @@ struct iOSApp: App {
 func extractDb() -> String {
     // Get the path to the Documents directory
     let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-
-    // Specify the destination path for the database in the Documents directory
-    let destinationURL = documentsDirectory.appendingPathComponent("app.db")
+    let outputFile = documentsDirectory.appendingPathComponent("database/app.db")
 
     // Check if the database file already exists in the Documents directory
-    if !FileManager.default.fileExists(atPath: destinationURL.path) {
+    if !FileManager.default.fileExists(atPath: outputFile.path) {
         // If it doesn't exist, copy it from the app bundle
-        if let bundleURL = Bundle.main.url(forResource: "app", withExtension: "db") {
+        if let bundlePath = Bundle.main.resourcePath {
+            let resourceDirectory = URL(fileURLWithPath: bundlePath)
+            let inputFile = resourceDirectory.appendingPathComponent("database/app.db")
+            
             do {
-                try FileManager.default.copyItem(at: bundleURL, to: destinationURL)
+                try FileManager.default.createDirectory(
+                    at: outputFile.deletingLastPathComponent(),
+                    withIntermediateDirectories: true,
+                    attributes: nil
+                )
+                
+                try FileManager.default.copyItem(at: inputFile, to: outputFile)
             } catch {
                 print("Error copying database: \(error)")
             }
         }
     }
     
-    return documentsDirectory.path
+    return outputFile.deletingLastPathComponent().path
 }
