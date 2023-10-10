78a016fcfe4df78576a21d222abacd59157f70ae
thach.do <ddthach95@gmail.com>
2023-10-11 01:08:42 +0800

    Set up navigation dropdown

--- shared/src/commonMain/kotlin/com/rdev/tt/ui/test_suite/TestSuiteCompact.kt
+++ shared/src/commonMain/kotlin/com/rdev/tt/ui/test_suite/TestSuiteCompact.kt
@@ -4,19 +4,24 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.RadioButtonChecked
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,9 +34,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -41,6 +50,7 @@ import com.rdev.tt.core_model.Category
 import com.rdev.tt.core_model.Question
 import com.rdev.tt.core_model.Suite
 import com.rdev.tt.ui.question.renderQuestion
+import kotlinx.coroutines.launch
 
 @Composable
 fun TestSuiteCompactScreen(
@@ -100,6 +110,8 @@ private fun TestSuiteCompactComp(
 ) {
     val userAnswers = remember { mutableStateMapOf<Long, Int>() }
     val pagerState = rememberPagerState(pageCount = { questions.size })
+    val coroutineScope = rememberCoroutineScope()
+    var isDropdownMenuExpanded by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier,
@@ -117,8 +129,37 @@ private fun TestSuiteCompactComp(
                     actions = {
                         Text(
                             "${pagerState.currentPage + 1} / ${questions.size}",
-                            Modifier.padding(end = Spacing.x4)
+                            modifier = Modifier
+                                .padding(end = Spacing.x4)
+                                .clickable { isDropdownMenuExpanded = true },
+                            textDecoration = TextDecoration.Underline
                         )
+
+                        DropdownMenu(
+                            expanded = isDropdownMenuExpanded,
+                            onDismissRequest = { isDropdownMenuExpanded = false }
+                        ) {
+                            questions.forEachIndexed { index, question ->
+                                DropdownMenuItem(
+                                    text = {
+                                        Text("${index + 1}")
+                                    },
+                                    onClick = {
+                                        isDropdownMenuExpanded = false
+                                        coroutineScope.launch { pagerState.scrollToPage(index) }
+                                    },
+                                    trailingIcon = {
+                                        if (question.id in userAnswers.keys) {
+                                            Icon(
+                                                Icons.Filled.RadioButtonChecked,
+                                                null,
+                                                Modifier.size(16.dp)
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        }
                     },
                 )
             }
